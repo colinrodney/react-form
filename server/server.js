28fde78fe42dce0e34a9e38551ad761b690676ab
Colin Rodney <colin.rodney@ibm.com>
2022-09-09 18:38:18 -0500

    adding routes (incomplete)

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const app = express()
+let testRoute = require("./routes/testRoutes")
 const port = 9000
 
 
-app.get("/", (req, res) =>{
-    res.send("welcome to express!")
-})
+app.use("/test", testRoute)
+// app.use("/", testRoute)
+// app.get("/", (req, res) =>{
+//     res.send("welcome to express!")
+// })
+
+// app.post("/", (req, res) =>{
+//     res.send(`registration successful`)
+// })
 
 app.listen(port, () =>{
     console.log(`app listening on port ${port}`)
