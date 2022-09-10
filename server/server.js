7f7a5d5d5ab32b0d8795e2cf0dd25ea1ab066473
colinrodney <c.renauld.rodney@gmail.com>
2022-09-09 23:41:30 -0500

    ensured index and test routes are correct

--- server/server.js
+++ server/server.js
@@ -1,18 +1,10 @@
 const express = require("express")
 const app = express()
-let testRoute = require("./routes/testRoutes")
+let index = require("./routes/home")
 const port = 9000
 
 
-app.use("/test", testRoute)
-// app.use("/", testRoute)
-// app.get("/", (req, res) =>{
-//     res.send("welcome to express!")
-// })
-
-// app.post("/", (req, res) =>{
-//     res.send(`registration successful`)
-// })
+app.use("/", index)
 
 app.listen(port, () =>{
     console.log(`app listening on port ${port}`)
