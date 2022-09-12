9af5b1c7d38adf22b50f35a7f9e2d2b07be78a5d
colinrodney <c.renauld.rodney@gmail.com>
2022-09-11 20:54:34 -0500

    connected and writing to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require("express")
 const app = express()
 let index = require("./routes/home")
+
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost:27017/test");
+const registrants = mongoose.model("registrants", {name: String})
+let testUser = new registrants({name: "Hal"})
+testUser.save().then(() => console.log(testUser.name))
+// mongoose.registrants.insertOne({name: testUser.name})
+
 const port = 9000
 
 
