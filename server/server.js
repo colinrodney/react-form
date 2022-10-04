dc4dcee00ae120304802afca4e60c6a3e9452174
Colin Rodney <colin.rodney@ibm.com>
2022-10-03 21:31:08 -0500

    commented out mongodb references @ server.js

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ const app = express()
 let indexRoute= require("./routes/getRoutes")
 let postRoute = require("./routes/postRoutes")
 
-const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost:27017/test");
-const registrants = mongoose.model("registrants", {name: String})
-let testUser = new registrants({name: "Hal"})
-testUser.save().then(() => console.log(testUser.name))
+// const mongoose = require("mongoose")
+// mongoose.connect("mongodb://localhost:27017/test");
+// const registrants = mongoose.model("registrants", {name: String})
+// let testUser = new registrants({name: "Hal"})
+// testUser.save().then(() => console.log(testUser.name))
 // mongoose.registrants.insertOne({name: testUser.name})
 
 const port = 9000
