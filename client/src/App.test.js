26ea4c03ac06f5ffb8bfaba1ba63fdd2e90df235
Colin Rodney <colin.rodney@ibm.com>
2022-11-10 19:43:50 -0600

    testing w/ react-testing-library

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+test("renders subscribe to mailing list form", () =>{
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  expect(screen.getByText(/subscribe for updates/i));
+})
