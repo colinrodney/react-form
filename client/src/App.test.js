710a71ea2c85676c06bb2e653e38a4c5791dad82
Colin Rodney <colin.rodney@ibm.com>
2022-11-10 20:18:09 -0600

    returning form to its original state

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,14 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-
-test("renders subscribe to mailing list form", () =>{
+test('renders learn react link', () => {
   render(<App />);
-  expect(screen.getByText(/subscribe for updates/i));
-})
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
